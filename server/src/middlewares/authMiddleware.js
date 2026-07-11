a0dcd3c6cb2da15ff53f5bee9b4e4346447e4366
Arjun Virk <asv0773@gmail.com>
2026-07-11 21:56:22 +0530

    stil updating login issue on icognito and iphone

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const protect = async (req, res, next) => {
+  console.log("Origin:", req.headers.origin);
+  console.log("Cookie Header:", req.headers.cookie);
+  console.log("Parsed Cookies:", req.cookies);
+
   try {
-    console.log("Cookies:", req.cookies);
     const token = req.cookies.token;
 
-    // CHECK TOKEN
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -13,16 +15,12 @@ export const protect = async (req, res, next) => {
       });
     }
 
-    // VERIFY TOKEN
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // SAVE USER ID
     req.user = decoded;
-    // console.log("COOKIE:", req.cookies);
-    // console.log("TOKEN:", req.cookies.jwt);
+
     next();
   } catch (error) {
-    res.status(401).json({
+    return res.status(401).json({
       success: false,
       message: "Invalid token",
     });
